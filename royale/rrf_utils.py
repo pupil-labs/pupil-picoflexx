2d8865564c116ea8317649d487ba3c00b02bb7ab
David McFarlane <david.mcfarlane@unsw.edu.au>
2019-06-26 10:51:16 +1000

    RrfHelper: Add some documentation

--- royale/rrf_utils.py
+++ royale/rrf_utils.py
@@ -1,4 +1,5 @@
 import struct
+from typing import Optional
 
 
 class RrfHelper:
@@ -16,7 +17,15 @@ class RrfHelper:
             self._f = None
             del self._f
 
-    def _read(self, fmt: str, offset: int = None):
+    def _read(self, fmt: str, offset: Optional[int] = None):
+        """
+        Reads data as specified in the format string, optionally seeking in
+        the file before reading.
+
+        :param fmt: The struct format string to read
+        :param offset: The optional offset to seek to in the file
+        :return: The data item read, or a list of such if there were multiple
+        """
         if offset is not None:
             self._f.seek(offset)
 
@@ -30,9 +39,14 @@ class RrfHelper:
         return unpacked
 
     def _load(self):
+        # The number of frames is an integer at offset 0x79
         self.num_frames = self._read('<I', 0x79)
+        # The byte offset to the first frame in is an integer at offset 0x8A
         self.offs_first_frame = self._read('<I', 0x8A)
 
+        # Each frame is prefixed with it's size,  in testing this has been
+        # constant if the frame size ends up being variable, the loop will
+        # need to be reworked.
         frame_sz = 0
         for f_idx in range(self.num_frames):
             info = self._read('<IHHHHHQ', self.offs_first_frame + frame_sz * f_idx)
